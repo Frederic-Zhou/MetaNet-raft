7c0dde8897f76e7b190187831e83afb8b1fd9bce
Frederic <zetachow@163.com>
2021-11-09 00:12:29 +0800

    定义rpc协议

--- raft/models.go
+++ raft/models.go
@@ -3,6 +3,22 @@
  */
 package models
 
+const (
+	Follower Role = iota
+	Candidate
+	Leader
+)
+
+// for each server, key is server's id,value is the server's index
+type ServerIndex map[string]uint64
+
+type Role int
+
+type Entry struct {
+	Term uint64
+	Data []byte
+}
+
 // Server
 ////////////////////////////////////////////////
 // Persistent state on all servers:
@@ -16,7 +32,7 @@ type Server struct {
 	Role Role
 
 	//latest term server has seen (initialized to 0 on first boot, increases monotonically)
-	CurrentTerm int64
+	CurrentTerm uint64
 
 	//candidateId that received vote in current term (or null if none)
 	VotedFor string
@@ -28,10 +44,10 @@ type Server struct {
 	Log []Entry
 
 	//index of highest log entry known to be committed (initialized to 0, increases monotonically)
-	CommitIndex int64
+	CommitIndex uint64
 
 	//index of highest log entry applied to state machine (initialized to 0, increases monotonically)
-	LastApplied int64
+	LastApplied uint64
 
 	//Volatile state on leaders:
 	//(Reinitialized after election)
@@ -42,49 +58,33 @@ type Server struct {
 	MatchIndex ServerIndex
 }
 
-// for each server, key is server's id,value is the server's index
-type ServerIndex map[string]int64
-
-type Entry struct {
-	Term int64
-	Data []byte
-}
-
-type Role int
-
-const (
-	Follower Role = iota
-	Candidate
-	Leader
-)
-
 ////////////////////////////////////////////////
 
 // AppendEntry
 //Invoked by leader to replicate log entries (§5.3); also used as heartbeat (§5.2).
 type AppendEntry struct {
 	//leader’s term
-	Term int64
+	Term uint64
 
 	//so follower can redirect clients
 	LeaderID string
 
 	//index of log entry immediately preceding new ones
-	PrevLogIndex int64
+	PrevLogIndex uint64
 
 	//term of prevLogIndex entry
-	PrevLogTerm int64
+	PrevLogTerm uint64
 
 	//Entries is a partment of Log
 	Entries []Entry
 
 	//leader’s commitIndex
-	LeaderCommit int64
+	LeaderCommit uint64
 }
 
 type AppendEntryResult struct {
 	//currentTerm, for leader to update itself
-	Term int64
+	Term uint64
 
 	//true if follower contained entry matching prevLogIndex and prevLogTerm
 	Success bool
@@ -94,21 +94,21 @@ type AppendEntryResult struct {
 //vote
 type Vote struct {
 	//candidate’s term
-	Term int64
+	Term uint64
 
 	//candidate requesting vote
 	CandidateID string
 
 	//index of candidate’s last log entry (§5.4)
-	LastLogIndex int64
+	LastLogIndex uint64
 
 	//term of candidate’s last log entry (§5.4)
-	LastLogTerm int64
+	LastLogTerm uint64
 }
 
 type VoteResult struct {
 	//currentTerm, for candidate to update itself
-	Term int64
+	Term uint64
 
 	//true means candidate received vote
 	VoteGranted bool
