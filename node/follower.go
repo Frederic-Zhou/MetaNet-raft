0283f1050cec861784bbacdc5b4a97c9042aa3d1
Frederic <zetachow@163.com>
2021-11-17 17:24:33 +0800

    增加打印

--- node/follower.go
+++ node/follower.go
@@ -67,7 +67,7 @@ func (f *Follower) AppendEntries(ctx context.Context, in *rpc.EntriesArguments) 
 	//追加日志中尚未存在的任何条目
 	f.Log = append(f.Log, in.GetEntries()...)
 
-	// logrus.Info(f.Log)
+	logrus.Info("write log", f.Log)
 
 	//同步Leader的CommitIndex
 	//note: 有可能出现本节点是较慢的节点，Leader已经提交了较高的Index，但是本节点未必获得了领导的最高提交Index的日志
