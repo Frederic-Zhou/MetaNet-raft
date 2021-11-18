6316bc6fae4992da68ba96060250be2c3b614641
Frederic <zetachow@163.com>
2021-11-18 12:20:51 +0800

    debug

--- node/statemachine.go
+++ node/statemachine.go
@@ -5,6 +5,7 @@ import (
 )
 
 func (n *Node) StateMachineHandler(data []byte) (err error) {
+	logrus.Warn(string(data))
 	switch {
 	case string(data[:4]) == "join" && n.CurrentRole != Role_Leader: //如果是join的log条目，写入到配置，并且如果是leader通知立刻对这个节点进行连接
 		id := string(data[4:])
